97e8d31bb8df01f0b89f6ba8824ff88afba26b2c
Marcos Tirao <marcos.tirao@icloud.com>
2022-04-07 16:01:05 -0300

    Update endpoint

--- src/patient_monitor.erl
+++ src/patient_monitor.erl
@@ -21,6 +21,7 @@ server(ClinicPid) ->
             notified_patient(NotificationCount, 0, Patients),
             no_data;
         {error, _} ->
+            io:format("Patient server unreachable.~n"),
             no_data
     end,
     timer:sleep(600000),
@@ -34,7 +35,7 @@ notification_count(Values) ->
 
 
 notified_patient(Count, Index, Values) ->
-  
+
     if 
         length(Values) > 0 ->
             if 
@@ -42,7 +43,7 @@ notified_patient(Count, Index, Values) ->
                     {ok, Status} = notify(hd(Values)),
                     if 
                         Status == sent -> notified_patient(Count, Index + 1, tl(Values));
-                        true -> notified_patient(Count, Index + 1, tl(Values))
+                        true -> notified_patient(Count, Index, tl(Values))
                     end;
                 true -> 
                     io:format("No more profile to notified~n")
@@ -54,6 +55,7 @@ notified_patient(Count, Index, Values) ->
 
 notify(Profile) ->
     ProfileId = maps:get(<<"profileid">>, Profile),
+    PatientId = maps:get(<<"patientid">>, Profile),
     {ok, Values} = notification_db_handler:query(ProfileId),
     if
         length(Values) > 0 -> 
@@ -61,20 +63,44 @@ notify(Profile) ->
             DateSec = calendar:datetime_to_gregorian_seconds(Date),
             DateSecCurrent = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
             TimeDifSec = (DateSecCurrent - DateSec) / 86400,
-            send_notification(TimeDifSec, ProfileId);
+            send_notification_with_frequency(TimeDifSec, ProfileId, PatientId);
         true -> 
-            notification_db_handler:create(ProfileId),
+            send_notification(ProfileId, PatientId),
             {ok, sent}
     end.
     
-send_notification(TimeDiff, ProfileId) ->
+send_notification_with_frequency(TimeDiff, ProfileId, PatientId) ->
     {ok, Values} = config_db_handler:query("notification_frequency"),
     {_, _, ParameterValue} = lists:nth(1, Values),
     ValueText = binary_to_list(ParameterValue),
     Frequency = list_to_integer(ValueText),
     if 
         TimeDiff > Frequency -> 
-            notification_db_handler:create(ProfileId),
+            send_notification(ProfileId, PatientId),
             {ok, sent};
         true -> {ok, notsent}
+    end.
+
+send_notification(ProfileId, PatientId) ->
+    {ok, ClinicPid} = gun:open("localhost", 4100),
+    RequestBody = jsone:encode(#{<<"sent">> => #{<<"profileid">> => ProfileId, <<"patientid">> => PatientId}}),
+    StreamRef = gun:post(ClinicPid, "/api/prevent/notified", [{<<"content-type">>, <<"application/json">>}], RequestBody),
+    case gun:await(ClinicPid, StreamRef) of
+        {response, fin, _, _} ->
+                no_data;
+        {response, nofin, ResponseCode, _} ->
+            if 
+                (ResponseCode >= 200) and (ResponseCode =< 299) ->
+                    {ok, Body} = gun:await_body(ClinicPid, StreamRef),
+                    Notification = jsone:decode(Body),
+                    io:format("Server response: ~p~n", [Notification]),
+                    notification_db_handler:create(ProfileId),
+                    no_data;
+                true ->
+                    io:format("Not valid server response~n"),
+                    no_data
+            end;
+        {error, _} ->
+            io:format("Patient server unreachable.~n"),
+            no_data
     end.
