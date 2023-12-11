9b2a783c32104d6755e67655b7fb25a212ae6372
Marcos Tirao <mtirao@chimlgbr00678.qvcuk.local>
2023-12-11 08:19:22 -0300

    Migrate to rebar3

--- apps/engine_rebar3/src/engine_rebar3_app.erl
+++ apps/engine_rebar3/src/engine_rebar3_app.erl
@@ -10,14 +10,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-    Dispatch = cowboy_router:compile([
-		{'_', [ {"/api/prevent/config", config_handler, []} ] } 
-    ]),
-    {ok, _} = cowboy:start_clear(my_http_listener,
-        [{port, 8081}],
-        #{env => #{dispatch => Dispatch}}
-	),
-	engine_sup:start_link().
+    %%Dispatch = cowboy_router:compile([
+		%%{'_', [ {"/api/prevent/config", config_handler, []} ] } 
+    %%]),
+    %%{ok, _} = cowboy:start_clear(my_http_listener,
+     %%   [{port, 8081}],
+     %%   #{env => #{dispatch => Dispatch}}
+	%%),
+	engine_rebar3_sup:start_link().
 
 stop(_State) ->
 	ok.
